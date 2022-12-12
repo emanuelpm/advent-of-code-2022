b98c57bbeaadb28ab61c4e43e467e8774115090c
Emanuel Masciarelli <emanuel@emanuelm.net>
2022-12-11 21:32:43 -0600

    Finishing day03

--- src/aoc2022/day03.rs
+++ src/aoc2022/day03.rs
@@ -9,15 +9,23 @@ pub fn day03() {
 	let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
+    // Going to treat compartments as bitfields since we don't care about
+    // the number of items in each type:
+    // Bits  1 - 26: a - z
+    // Bits 27 - 52: A - Z
+
+    // [Part 1]
     let mut priorities : u64 = 0;
+
+    // [Part 2]
+    let mut group_overlaps : u64 = 0;
+    let mut grouped_elf_priorities : u64 = 0;
+
+    let mut line_number : u64 = 1;      // We'll increment at the end of the loop
     for line in reader.lines() {
     	let line = line.unwrap();
     	let len = line.len();
 
-    	// Going to treat compartments as bitfields since we don't care about
-    	// the number of items in each type:
-    	// Bits  1 - 26: a - z
-    	// Bits 27 - 52: A - Z
     	let mut first_compartment : u64 = 0;
     	let mut second_compartment : u64 = 0;
 
@@ -39,15 +47,37 @@ pub fn day03() {
     		index += 1;
     	}
 
-    	// Any set bits in both are overlaps
-    	let compartment_overlap : u64 = first_compartment & second_compartment;
+    	let find_priorities = |value: u64| {
+            let mut priorities : u64 = 0;
+            for index in 1..=52 {
+        		if (value & (1 << index)) != 0 {
+        			priorities += index;
+        		}
+            }
 
-    	for index in 1..=52 {
-    		if (compartment_overlap & (1 << index)) != 0 {
-    			priorities += index;
-    		}
-    	}
+            return priorities;
+        };
+
+        // [Part 1] Any set bits in both are overlaps
+        priorities += find_priorities(first_compartment & second_compartment);
+
+        // [Part 2] We treat both compartments as one bag (or together)
+        if line_number % 3 == 1 {
+            // First line so we need to set the overlaps to the current setup
+            group_overlaps = first_compartment | second_compartment; 
+        }
+        else {
+            // Not the first line so we can just and them in
+            group_overlaps &= first_compartment | second_compartment;
+        }
+
+        if line_number % 3 == 0 {
+            grouped_elf_priorities += find_priorities(group_overlaps);
+        }
+
+        line_number += 1;
     }
 
     println!("[Part 1] Sum of priorities: {}", priorities);
+    println!("[Part 2] Sum of priorities: {}", grouped_elf_priorities);
 }
