a6d9428e4d5146a85638b0f2c7fa1b83f9acc53e
Emanuel Masciarelli <emanuel@emanuelm.net>
2022-12-11 21:38:05 -0600

    Finishing day04 part 2

--- src/aoc2022/day04.rs
+++ src/aoc2022/day04.rs
@@ -9,7 +9,8 @@ pub fn day04() {
 	let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    let mut num_overlaps : u64 = 0;
+    let mut num_total_overlaps : u64 = 0;
+    let mut num_any_overlaps : u64 = 0;
     for line in reader.lines() {
         let line = line.unwrap();
 
@@ -17,8 +18,12 @@ pub fn day04() {
         let elves = line.split(',').collect::<Vec<&str>>();
 
         // Going to treat the sections as bits so we can just and the elves
-        // together, and if the anded value matches one of the elves then
+        // together
+
+        // [Part 1] if the anded value matches one of the elves then
         // that means its rooms were completely taken care of by the other
+
+        // [Part 2] if the anded value isn't 0 there is some overlap
 
         // First elf (left side)
         let range = elves[0].split('-').collect::<Vec<&str>>();
@@ -41,10 +46,18 @@ pub fn day04() {
         }
 
         let overlap : u128 = elf1 & elf2;
+
+        // [Part 1] Checking for a full overlap
         if overlap == elf1 || overlap == elf2 {
-            num_overlaps += 1;
+            num_total_overlaps += 1;
+        }
+
+        // [Part 2] Checking for any overlap
+        if overlap != 0 {
+            num_any_overlaps += 1;
         }
     }
 
-    println!("[Part 1] Number of overlaps: {}", num_overlaps);
+    println!("[Part 1] Number of total overlaps: {}", num_total_overlaps);
+    println!("[Part 2] Number of partial overlaps: {}", num_any_overlaps);
 }
