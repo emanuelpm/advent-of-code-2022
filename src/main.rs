56ae39c4b10d4944d9a428775e660f3b89e9b092
Emanuel Masciarelli <emanuel@emanuelm.net>
2023-10-23 20:42:24 -0500

    Changing to not support multiple years
    I want to use a different languge every year, so removing that functionality.

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,10 @@ mod aoc2022;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        panic!("Expected 2 arguments: <year> <day>."); 
+    if args.len() != 2 {
+        panic!("Expected 1 argument: <day>.");
     }
 
-    let year = args[1].trim().parse().expect("Year must be an integer.");
-    let day = args[2].trim().parse().expect("Day must be an integer.");
-
-    match year {
-        2022 => aoc2022::run(day),
-        _ => panic!("No programs found for year {}", year),
-    }
+    let day = args[1].trim().parse().expect("Day must be an integer.");
+    aoc2022::run(day);
 }
